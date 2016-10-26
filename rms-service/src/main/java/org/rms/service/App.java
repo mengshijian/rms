66e0c85074fdfe31baea6fa7d02b581ad1241e03
ymsj <ymsj@DESKTOP-TGG899G>
2016-10-26 23:36:44 +0800

    初建项目

---
+++ rms-service/src/main/java/org/rms/service/App.java
@@ -0,0 +1,13 @@
+package org.rms.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
